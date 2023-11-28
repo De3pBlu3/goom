952510b9ff73c0ee3d2074b05d82d14f526c5c42
23357614 <jackcasey614@gmail.com>
2023-11-28 12:20:22 +0000

    init

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(3, Calculator.add(1, 2));
+        assertEquals(-4, Calculator.add(-6, 2));
+        assertEquals(0, Calculator.add(0, 0));
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(1, Calculator.subtract(3, 2));
+        assertEquals(-4, Calculator.subtract(-2, 2));
+        assertEquals(0, Calculator.subtract(0, 0));
+        assertEquals(2, Calculator.subtract(2, 0));
+    }
+}
